8f490e48c94f2d60ba3f2508ffae1e22a1b8499f
Ian Salas López <96995669+Salasian@users.noreply.github.com>
2023-02-16 20:06:52 -0700

    Actividad con sinopsis completa

--- PopCorn/app/src/main/java/salas/ian/popcorn/MovieDetail.kt
+++ PopCorn/app/src/main/java/salas/ian/popcorn/MovieDetail.kt
@@ -1,11 +1,32 @@
 package salas.ian.popcorn
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
 
 class MovieDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
+
+        var image = intent.getIntExtra("image", 0)
+        var titulo = intent.getStringExtra("titulo")
+        var header = intent.getIntExtra("header", 0)
+        var sinopsis = intent.getStringExtra("sinopsis")
+
+        var detailImage = findViewById<ImageView>(R.id.detail_view)
+        var detailTitle = findViewById<TextView>(R.id.detail_title)
+        var detailDescription = findViewById<TextView>(R.id.detail_desc)
+
+        detailImage.setImageResource(image)
+        detailTitle.text = titulo
+        detailDescription.text = sinopsis
     }
 }
